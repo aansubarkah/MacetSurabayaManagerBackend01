0d07fcbe6d0cd53de3e65951e33ba60d9d0bc6f8
aansubarkah <aansubarkah@gmail.com>
2015-08-31 11:36:07 +0700

    Places RD

--- src/Controller/PlacesController.php
+++ src/Controller/PlacesController.php
@@ -10,6 +10,29 @@ use App\Controller\AppController;
  */
 class PlacesController extends AppController
 {
+    public function beforeFilter(Event $event)
+    {
+        //parent::beforeFilter($event);
+        //$this->Auth->allow(['add', 'token']);
+        //$this->Auth->allow(['token']);
+    }
+
+    public $limit = 25;
+
+    public $paginate = [
+        'fields' => ['Places.id', 'Places.name', 'Places.active'],
+        'limit' => 25,
+        'page' => 0,
+        'order' => [
+            'Places.name' => 'asc'
+        ]
+    ];
+
+    public function initialize()
+    {
+        parent::initialize();
+        $this->loadComponent('Paginator');
+    }
 
     /**
      * Index method
@@ -18,8 +41,90 @@ class PlacesController extends AppController
      */
     public function index()
     {
-        $this->set('places', $this->paginate($this->Places));
-        $this->set('_serialize', ['places']);
+        $limit = $this->limit;
+        if (isset($this->request->query['limit'])) {
+            if (is_numeric($this->request->query['limit'])) {
+                $limit = $this->request->query['limit'];
+            }
+        }
+
+        if (isset($this->request->query['searchName'])) {
+            $searchName = trim($this->request->query['searchName']);
+            $this->checkExistence($searchName, $limit);
+        } else {
+            $page = 1;
+            $offset = 0;
+            if (isset($this->request->query['page'])) {
+                if (is_numeric($this->request->query['page'])) {
+                    $page = (int)$this->request->query['page'];
+                    $offset = ($page - 1) * $limit;
+                }
+            }
+
+            $query = '';
+            if (isset($this->request->query['query'])) {
+                if (!empty(trim($this->request->query['query']))) {
+                    $query = trim($this->request->query['query']);
+                }
+            }
+
+            $conditions = ['Places.active' => true];
+
+            if (!empty(trim($query))) {
+                $conditions['LOWER(Places.name) LIKE'] = '%' . strtolower($query) . '%';
+            }
+
+            $places = $this->Places->find()
+                ->where($conditions)
+                ->order(['Places.name' => 'ASC'])
+                ->limit($limit)->page($page)->offset($offset)
+                ->toArray();
+
+            $allPlaces = $this->Places->find()->where($conditions);
+            $total = $allPlaces->count();
+
+            $meta = [
+                'total' => $total
+            ];
+            $this->set([
+                'places' => $places,
+                'meta' => $meta,
+                '_serialize' => ['places', 'meta']
+            ]);
+        }
+    }
+
+    public function checkExistence($name = null, $limit = 25)
+    {
+        $data = [
+            [
+                'id' => 0,
+                'name' => '',
+                'active' => 0
+            ]
+        ];
+
+        $fetchDataOptions = [
+            'order' => ['Places.name' => 'ASC'],
+            'limit' => $limit
+        ];
+
+        $query = trim(strtolower($name));
+
+        if (!empty($query)) {
+            $fetchDataOptions['conditions']['LOWER(Places.name) LIKE'] = '%' . $query . '%';
+        }
+
+        $place = $this->Places->find('all', $fetchDataOptions);
+
+        if ($place->count() > 0) {
+            $data = $place;
+        }
+
+        $this->set([
+            'place' => $data,
+            '_serialize' => ['place']
+        ]);
     }
 
     /**
@@ -93,13 +198,18 @@ class PlacesController extends AppController
      */
     public function delete($id = null)
     {
-        $this->request->allowMethod(['post', 'delete']);
         $place = $this->Places->get($id);
-        if ($this->Places->delete($place)) {
-            $this->Flash->success(__('The place has been deleted.'));
-        } else {
-            $this->Flash->error(__('The place could not be deleted. Please, try again.'));
+        if ($this->request->is(['delete'])) {
+            $place->active = false;
+            if ($this->Places->save($place)) {
+                $message = 'Deleted';
+            } else {
+                $message = 'Error';
+            }
         }
-        return $this->redirect(['action' => 'index']);
+        $this->set([
+            'place' => $place,
+            '_serialize' => ['place']
+        ]);
     }
 }
